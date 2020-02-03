92f7e4318b49b9a448b22c8fdb85de85559def2a
Angelo Shema <geoangercola@gmail.com>
2020-02-03 18:34:34 +0200

    Added Uglifier(harmony: true) for js compressor, removed consoles in layouts
    On branch master
    Your branch is up to date with 'origin/master'.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,6 +44,12 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
+  config.assets.js_compressor = Uglifier.new(harmony: true)
+  # config.assets.css_compressor = :sass
+
+  # Do not fallback to assets pipeline if a precompiled asset is missed.
+  config.assets.compile = true
+
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
